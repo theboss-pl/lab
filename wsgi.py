fe80aefc2d1142e0fa45bc5eef2f7b2a09965469
theboss-pl <theboss@op.pl>
2020-03-21 23:35:32 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -14,6 +14,26 @@ def db_connect():
     conn.close()
     return str(ver)
 
+def db_connect_2():
+    conn = psycopg2.connect(host="172.30.130.175", user="test", password="test123", dbname="sampledb")
+    ver = conn.server_version
+#    cursor = conn.cursor()
+#    cursor.execute('SELECT VERSION()')
+#    row = cursor.fetchone()
+#    cursor.close()
+    conn.close()
+    return str(ver)
+
+def db_connect_url():
+    conn = psycopg2.connect(host="ip-10-0-146-246.ec2.internal", user="test", password="test123", dbname="sampledb")
+    ver = conn.server_version
+#    cursor = conn.cursor()
+#    cursor.execute('SELECT VERSION()')
+#    row = cursor.fetchone()
+#    cursor.close()
+    conn.close()
+    return str(ver)
+
 @application.route("/")
 def hello():
     return "Hey! Now it's " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -21,6 +41,14 @@ def hello():
 @application.route("/version")
 def version():
     return db_connect()
+
+@application.route("/version2")
+def version():
+    return db_connect_2()
+
+@application.route("/ver")
+def version():
+    return db_connect_url()
 
 if __name__ == "__main__":
     application.run()
