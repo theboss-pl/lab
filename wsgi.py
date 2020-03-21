91807485ea3488ce5b15b5b4c0803d7fb64444fe
theboss-pl <theboss@op.pl>
2020-03-21 18:55:32 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,8 +5,9 @@ application = Flask(__name__)
 
 def db_connect:
     myConnection = psycopg2.connect(host="10.129.13.10", user="test", password="test123", dbname="sampledb")
+    ver = myConnection.server_version
     myConnection.close()
-    return "Zażółć gęślą jaźń!"
+    return "Zażółć gęślą jaźń! " + str(ver)
 
 @application.route("/")
 def hello():
