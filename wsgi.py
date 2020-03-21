6f8f9852297bc87df373485be625722a0d74347b
theboss-pl <theboss@op.pl>
2020-03-21 19:00:45 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,13 @@ import psycopg2
 application = Flask(__name__)
 
 def db_connect:
-    myConnection = psycopg2.connect(host="10.129.13.10", user="test", password="test123", dbname="sampledb")
+    conn = psycopg2.connect(host="10.129.13.10", user="test", password="test123", dbname="sampledb")
     ver = myConnection.server_version
-    myConnection.close()
+    cursor = conn.cursor()
+    cursor.execute('SELECT VERSION()')
+    row = cursor.fetchone()
+    cursor.close()
+    conn.close()
     return "Zażółć gęślą jaźń! " + str(ver)
 
 @application.route("/")
