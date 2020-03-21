5f15d5a7c6929cf2004d28b2a77f921381a2f484
theboss-pl <theboss@op.pl>
2020-03-21 23:06:06 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,26 @@
 from flask import Flask
 from datetime import datetime
-#import psycopg2
+import psycopg2
 
 application = Flask(__name__)
 
-#def db_connect:
-#    conn = psycopg2.connect(host="10.129.13.10", user="test", password="test123", dbname="sampledb")
-#    ver = conn.server_version
+def db_connect:
+    conn = psycopg2.connect(host="10.129.13.10", user="test", password="test123", dbname="sampledb")
+    ver = conn.server_version
 #    cursor = conn.cursor()
 #    cursor.execute('SELECT VERSION()')
 #    row = cursor.fetchone()
 #    cursor.close()
-#    conn.close()
-#    return "Hello ver=" + str(ver)
+    conn.close()
+    return str(ver)
 
 @application.route("/")
 def hello():
-    return "Hello! Now it's " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return "Hey! Now it's " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+@application.route("/version")
+def version():
+    return db_connect()
 
 if __name__ == "__main__":
     application.run()
